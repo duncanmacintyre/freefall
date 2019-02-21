f8c591fe528536e0768c30e2171e26dfeee00ce1
Duncan MacIntyre <duncanmacintyre@users.noreply.github.com>
2019-02-21 11:26:30 -0800

    Readd testing.py
    Need to resolve syncing

--- testing.py
+++ testing.py
@@ -5,9 +5,13 @@ import numpy
 import pandas
 import matplotlib.pyplot as plt
 import freefall
+from time import sleep
 
-df = freefall.box_sim(l=1.8, h=4, w=0.4, m=10, W_x=4, W_y=0.3, W_z=0.1, start=0, stop=10, dt=0.0001)
+df = freefall.box_sim(l=6, h=0.2, w=0.4, m=10, W_x=4, W_y=0.3, W_z=0.1, start=0, stop=10, dt=0.0001)
 
-fig, ax = freefall.plot(df, 0, 10, 'The first ten seconds')
+fig, ax = freefall.plot(df, 0, 10, 'The first ten seconds', sharex=True)
 plt.show()
-#freefall.visualize_df(df, 1.8, 4, 0.4, speed=0.1, caption='This is the caption.', stop=10)
+
+# import vpython
+# sleep(7)
+# freefall.visualize_df(df, 1.8, 4, 0.4, speed=0.04, stop=2)
